132c4445552c260167d441812565c890c37656ca
innazinchenko <spangle_red@inbox.ru>
2025-01-29 20:57:10 +0100

    first commit

---
+++ src/main/java/task_01/FirstClass.java
@@ -0,0 +1,7 @@
+package task_01;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("First class");
+    }
+}
---
+++ src/main/java/task_01/SecondClass.java
@@ -0,0 +1,7 @@
+package task_01;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Second class");
+    }
+}
