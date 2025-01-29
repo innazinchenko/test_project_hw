21beccec151fbdafa9b2cbbe38e63d4f527c173c
innazinchenko <spangle_red@inbox.ru>
2025-01-29 21:17:37 +0100

    third commit

--- src/main/java/task_01/FirstClass.java
+++ src/main/java/task_01/FirstClass.java
@@ -3,5 +3,9 @@ package task_01;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First class");
+        System.out.println("First class");
+        System.out.println("First class");
+        System.out.println("First class");
+        System.out.println("First class");
     }
 }
--- src/main/java/task_01/SecondClass.java
+++ src/main/java/task_01/SecondClass.java
@@ -3,5 +3,9 @@ package task_01;
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Second class");
+        System.out.println("Second class");
+        System.out.println("Second class");
+        System.out.println("Second class");
+        System.out.println("Second class");
     }
 }
